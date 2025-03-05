200a381cfe9c7d8f24f8b3f86173eebe231f14d9
Nils Roberts Krištopāns <A230756NK@rvt.lv>
2025-03-05 16:52:24 +0200

    Configured the JavaFX controller - the app opens on launch.

--- src/main/java/wickedbet/Main.java
+++ src/main/java/wickedbet/Main.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("..."));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/javafx/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setScene(scene);
+        stage.setTitle("Wicked Bet");
         stage.show();
     }
 
--- src/main/java/wickedbet/controllers/MainController.java
+++ src/main/java/wickedbet/controllers/MainController.java
@@ -1,8 +1,19 @@
 package wickedbet.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MainController implements Initializable {
+    @FXML
+    private Label label;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        label.setText("Welcome to WICKED BET!");
+    }
 }
