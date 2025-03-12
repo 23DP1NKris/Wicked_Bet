80adc7180b7fd7ad3bf5d78c54f41ec36c588b3e
Nils Roberts Krištopāns <A230756NK@rvt.lv>
2025-03-12 11:04:49 +0200

    Added content to the LoginController class.

--- src/main/java/wickedbet/controllers/LoginController.java
+++ src/main/java/wickedbet/controllers/LoginController.java
@@ -1,17 +1,41 @@
 package wickedbet.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
-    public void signIn(ActionEvent e) {
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button registerText;
+
+    public LoginController(TextField username) {
+        this.username = username;
+        this.password = new PasswordField();
+    }
+
+    @FXML
+    private void loginButtonAction(ActionEvent event) {
 
     }
 
+    @FXML
+    private void registerTextAction(ActionEvent event) {
+
+    }
+
+    @FXML
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
