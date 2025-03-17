6aa5ce6ce61aaca7531735063a92677e1b1a4188
Nils Roberts Krištopāns <A230756NK@rvt.lv>
2025-03-17 21:32:48 +0200

    Gave functionality to both the "Register an Acccount" and "Log in" text buttons.

--- src/main/java/wickedbet/controllers/LoginController.java
+++ src/main/java/wickedbet/controllers/LoginController.java
@@ -1,7 +1,7 @@
 package wickedbet.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,17 +9,21 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 
-public class LoginController{
-
-    @FXML
+public class LoginController implements Initializable {
     public void switchToRegister(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("register.fxml")));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/javafx/register.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
--- src/main/java/wickedbet/controllers/RegisterController.java
+++ src/main/java/wickedbet/controllers/RegisterController.java
@@ -1,23 +1,25 @@
 package wickedbet.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class RegisterController implements Initializable {
-
-    @FXML
-    private Button registerButton;
-
-    @FXML
-    private Button logInText;
-
-    public RegisterController(TextField username, PasswordField password) {
-
+    public void switchToLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/javafx/login.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     @FXML
