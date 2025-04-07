5563eacf1032b788ed68b279bd3104ae421c4e54
Nils Roberts Krištopāns <A230756NK@rvt.lv>
2025-04-07 09:02:29 +0300

    Added a login validation and readjusted both classes.

--- src/main/java/wickedbet/controllers/LoginController.java
+++ src/main/java/wickedbet/controllers/LoginController.java
@@ -1,43 +1,34 @@
 package wickedbet.controllers;
 
-import wickedbet.services.LoginService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import wickedbet.services.LoginService;
 
 import java.io.IOException;
 
 public class LoginController {
-
     @FXML
     private TextField usernameField;
-
     @FXML
     private PasswordField passwordField;
 
     private final LoginService loginService = new LoginService();
     private final SceneController sceneController = new SceneController();
 
-    @FXML
     public void login(ActionEvent event) throws IOException {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
-        if (username.isEmpty() || password.isEmpty()) {
-            System.out.println("Username or password cannot be empty.");
-            return;
-        }
+        String username = usernameField.getText().trim();
+        String password = passwordField.getText().trim();
 
         if (loginService.loginUser(username, password)) {
             sceneController.switchToMenu(event);
-        } else {
-            System.out.println("Invalid login credentials.");
         }
     }
 
     @FXML
-    public void switchToRegister(ActionEvent event) throws IOException {
+    private void switchToRegister(ActionEvent event) throws IOException {
         sceneController.switchToRegister(event);
     }
+
 }
--- src/main/java/wickedbet/services/LoginService.java
+++ src/main/java/wickedbet/services/LoginService.java
@@ -7,18 +7,31 @@ import java.util.List;
 
 public class LoginService {
     private final JsonService jsonService = new JsonService();
-    private final UserAlerts LoginAlerts = new UserAlerts();
+    private final UserAlerts loginAlerts = new UserAlerts();
+    private final ValidInputService validInputService = new ValidInputService();
 
     public boolean loginUser(String username, String password) {
-        List<User> users = jsonService.loadUsers();
+        if (!validationCheck(username, password)) {
+            return false;
+        }
 
+        List<User> users = jsonService.loadUsers();
         for (User user : users) {
             if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
                 return true;
             }
         }
 
-        LoginAlerts.showAlert("Invalid credentials", "Invalid username or password.");
+        loginAlerts.showAlert("Invalid credentials", "Invalid username or password.");
         return false;
+    }
+
+    public boolean validationCheck(String username, String password) {
+        if (validInputService.emptyInputs(username, password)) {
+            loginAlerts.showAlert("Invalid input", "Username or password fields cannot be empty!");
+            return false;
+        }
+
+        return true;
     }
 }
