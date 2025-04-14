c28f9347ccafc6fa62265222795e635c9406a263
Nils Roberts Krištopāns <A230756NK@rvt.lv>
2025-04-14 22:29:17 +0300

    Fixed regex - not being able to register with capital letters or numbers.

--- src/main/java/wickedbet/services/ValidInputService.java
+++ src/main/java/wickedbet/services/ValidInputService.java
@@ -8,7 +8,7 @@ public class ValidInputService {
     private final JsonService jsonService = new JsonService();
 
     public static boolean validInputs(String username, String password) {
-        return !Pattern.matches("^[a-z0-A-Z9_]+$", username) || !Pattern.matches("^[a-zA-Z0-9!@#$&_]+$", password);
+        return !Pattern.matches("^[a-zA-Z0-9_]+$", username) || !Pattern.matches("^[a-zA-Z0-9!@#$&_]+$", password);
     }
 
     public boolean emptyInputs(String username, String password) {
